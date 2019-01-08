ae0034334d5f75fb0b32bf154cf204e6cd1ee1f0
Filip Tepper <filip@tepper.pl>
2019-01-08 12:39:55 +0100

    initial commit

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,10 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_rundeck_installed(File):
+    assert File("/etc/init.d/rundeckd").exists
